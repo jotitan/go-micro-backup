7b293b20110eedc98343049d61ff888c1e9dbfb7
jonathan.baranzini <titanbar@hotmail.com>
2021-03-19 16:02:43 +0100

    Add trace log

--- src/watcher/rest_opertions.go
+++ src/watcher/rest_opertions.go
@@ -3,6 +3,7 @@ package watcher
 import (
 	"fmt"
 	"github.com/jotitan/fsnot-poc/config"
+	"log"
 	"net/http"
 	"net/url"
 )
@@ -17,20 +18,29 @@ func NewRestOperationManager(conf config.Config)OperationManager {
 }
 
 func (rom restOperationManager) CopyFile(path string) {
+	log.Println("Rest Copy File",path)
 	restUrl := fmt.Sprintf("%s/file",rom.url)
 	values := url.Values{"path":{path}}
-	http.DefaultClient.PostForm(restUrl,values)
+	if _,err := http.DefaultClient.PostForm(restUrl,values) ; err != nil {
+		log.Println("Error",err)
+	}
 }
 
 func (rom restOperationManager) CopyFolder(path string) {
+	log.Println("Rest Copy Folder",path)
 	restUrl := fmt.Sprintf("%s/folder",rom.url)
 	values := url.Values{"path":{path}}
-	http.DefaultClient.PostForm(restUrl,values)
+	if _,err := http.DefaultClient.PostForm(restUrl,values) ; err != nil {
+		log.Println("Error",err)
+	}
 }
 
 func (rom restOperationManager) Delete(path string){
+	log.Println("Delete",path)
 	restUrl := fmt.Sprintf("%s/path=%s",rom.url,path)
 	r,_ := http.NewRequest(http.MethodDelete,restUrl,nil)
-	http.DefaultClient.Do(r)
+	if _,err := http.DefaultClient.Do(r); err != nil {
+		log.Println("Error",err)
+	}
 }
 
